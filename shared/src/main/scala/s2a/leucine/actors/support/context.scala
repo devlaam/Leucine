a389352e887094ca914647f9415d6afcb9749bed
devlaam <ruud@betaresearch.nl>
2023-01-31 15:35:35 +0100

    Added method to manually delay a task.

--- shared/src/main/scala/s2a/leucine/actors/support/context.scala
+++ shared/src/main/scala/s2a/leucine/actors/support/context.scala
@@ -1,26 +1,34 @@
 package s2a.leucine.actors
 
+import java.util.concurrent.Callable
+
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.Try
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.duration.DurationInt
 
 /**
- * An extended `scala.concurrent.ExecutionContext`; provides the ability to
- * schedule messages to be sent later, and hooks to track the current number of
- * outstanding tasks or log the actor message sends for debugging purposes
- */
+  * An extended `scala.concurrent.ExecutionContext`; provides the ability to
+  * schedule messages to be sent later, and hooks to track the current number of
+  * outstanding tasks or log the actor message sends for debugging purposes */
 trait ActorContext extends PlatformContext with ExecutionContext :
 
   /** Helper value to switch on/off tracing for debugging. Start your debug lines with
     * if trace then ... */
   val trace = false
 
+  /** Future to be executed on the ActorContext. */
   def future[T](task: => T): Future[T] =
     val promise = Promise[T]()
     def runnable = new Runnable { def run() = promise.complete(Try(task)) }
     execute(runnable)
     promise.future
+
+  /** Delayed task to be executed on the ActorContext. */
+  def delayed(task: => Unit, time: FiniteDuration): Cancellable =
+    val callable = new Callable[Unit] { def call() = task }
+    schedule(callable,time)
+
 
   def reportFailure(cause: Throwable): Unit = cause.printStackTrace()
 
