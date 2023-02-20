44c1181692fd014f7e0dfd93a32f075a588a9130
devlaam <ruud@betaresearch.nl>
2023-02-20 22:54:04 +0100

    Changed timing methods + bugfix

--- shared/src/main/scala/s2a/leucine/actors/mixins/timing.scala
+++ shared/src/main/scala/s2a/leucine/actors/mixins/timing.scala
@@ -64,7 +64,11 @@ trait TimingActor(using context: ActorContext) extends ActorDefs :
    * Construct a new callable on the fly. The call method puts the event on the events queue and
    * calls trigger in order to start the loop if needed. */
   private def callable(event: Event[MyLetter]) =
-    def handle() = synchronized {
+    /* The handle takes an achored event and puts it on the event queue. Due to the synchronization
+     * eventsCancel() and handle cannot interfere. But it is possible that the call() was made before
+     * the cancel but the handle afterwards. In that situation cancel() did miss its effect.
+     * So we may only proceed if the anchor is still present. */
+    def handle() = synchronized { if anchors.contains(event.anchor) then
       events.enqueue(event)
       anchors.remove(event.anchor)
       processTrigger() }
@@ -104,12 +108,16 @@ trait TimingActor(using context: ActorContext) extends ActorDefs :
    * A letter is send to myself after the delay. If there is already a letter posted under this
    * anchor, it will replace that letter. The original is guaranteed not to arrive, even if the
    * timer has already expired during execution of this letter. If the actor was asked to finish,
-   * the letter will NOT be posted AND the original letter is removed as well. */
-  protected def post(letter: MyLetter, delay: FiniteDuration, anchor: Object = this): Unit = synchronized {
+   * the letter will NOT be posted AND the original letter is removed as well, if present.
+   * Returns if the post was accepted. */
+  protected def post(letter: MyLetter, delay: FiniteDuration, anchor: Object = this): Boolean = synchronized {
     /* First remove a post that may be out there. */
     dump(anchor)
-    /* Then schedule a new timer and add it to the achors map. */
-    if isActive then anchors.addOne(anchor -> context.schedule(callable(Event(anchor,letter)),delay)) }
+    /* If we are not active, we may not accept this post. Otherwise ... */
+    if !isActive then false else
+      /* ... schedule a new timer and add it to the achors map. */
+      anchors.addOne(anchor -> context.schedule(callable(Event(anchor,letter)),delay))
+      true }
 
   /**
    * Stop a scheduled letter, after dump() it is guaranteed that no letters will arive on this
@@ -131,13 +139,16 @@ trait TimingActor(using context: ActorContext) extends ActorDefs :
    * not block, if it does, it will hold the current thread. This may also hold the whole
    * application in a single threaded environment. As long as the event did not yet arrive,
    * fullfill should produce None asap. It will be probed somewhat later once more. If the actor
-   * was asked to finish, the request will be ignored AND the any former timer/expectation on
-   * this anchor is cleared as well. */
-  protected def expect(fullfil: => Option[MyLetter], anchor: Object = this): Unit = synchronized {
+   * was asked to finish, the request will be ignored AND any former timer/expectation on
+   * this anchor is cleared as well, if present. Returns if the expectation was accepted. */
+  protected def expect(fullfil: => Option[MyLetter], anchor: Object = this): Boolean = synchronized {
     /* First remove prior anchor use. */
     dump(anchor)
-    /* Then schedule a new expectation and add it to the achors map. */
-    if isActive then anchors.addOne(anchor -> context.await(digestable(anchor),fullfil)) }
+    /* If we are not active, we may not accept this expectation. Otherwise ... */
+    if !isActive then false else
+    /* ... schedule a new expectation and add it to the achors map. */
+      anchors.addOne(anchor -> context.await(digestable(anchor),fullfil))
+      true }
 
 
 object TimingActor :
