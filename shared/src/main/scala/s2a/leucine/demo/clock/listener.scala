750e9636c11047a5594a478937d89e5f42288ec6
devlaam <ruud@betaresearch.nl>
2023-04-29 19:04:59 +0200

    Renamed Timer to Listener

--- shared/src/main/scala/s2a/leucine/demo/clock/listener.scala
+++ shared/src/main/scala/s2a/leucine/demo/clock/listener.scala
@@ -32,24 +32,24 @@ import s2a.leucine.actors.*
 
 /* This class captures the incoming connection an creates a new actor for each. It is derived from
  * AcceptActor because we do no receive letters from others than ourselves. As a best practice we define
- * the letters Timer is able to process in the companion object, and use Timer.Letter as the base
+ * the letters Listener is able to process in the companion object, and use Listener.Letter as the base
  * type. Furthermore we need to mix TimingAid in for we need letters that arrive after a certain
  * period (with 'post') is over as well as the ability to wait for an i/o event (with 'expect').
  * Since this Actor spawns other other we want to automatically terminate when it stops, we make it
  * root of the family. Direct children of this actor may receive letters of the type Provider.Letter. */
-class Timer extends RestrictActor(Timer,"server"), TimingAid, FamilyRoot(Timer), LogInfo :
+class Listener extends RestrictActor(Listener,"server"), TimingAid, FamilyRoot(Listener), LogInfo :
 
   /* Time this demo will last. */
   val runtime = 60.seconds
 
-  println(s"Timer Constructed, will run for $runtime.")
+  println(s"Listener Constructed, will run for $runtime.")
 
   /* We use different timer methods, which need different anchors so they do not overwrite each other. */
   val terminationAnchor = new Object
   val expectationAnchor = new Object
 
   /* Make sure this server ends after 60 seconds. It is just for testing. */
-  post(Timer.Terminated,runtime,terminationAnchor)
+  post(Listener.Terminated,runtime,terminationAnchor)
 
   /* First create the generalized serverSocket. This should not fail. */
   private val serverSocket: ServerSocket = new ServerSocketImplementation
@@ -59,23 +59,23 @@ class Timer extends RestrictActor(Timer,"server"), TimingAid, FamilyRoot(Timer),
    * arrives we send a letter to ourselves with the connection enclosed. */
   private val useCallback = serverSocket.onConnect(socket =>
     Logger.debug("Callback called.")
-    send(Timer.Connect(socket),Actor.Anonymous))
+    send(Listener.Connect(socket),Actor.Anonymous))
 
   /* See if there anyone knocking on the door. We need this if there is no callback
    * function on the platform available. */
-  def connect: Option[Timer.Letter[Timer.Accept]] =
+  def connect: Option[Listener.Letter[Listener.Accept]] =
     /* Test if we have a request for a connection. */
     serverSocket.request()
     /* This may result in an error, if ... */
     if serverSocket.error.isEmpty
     /* ... not map the connection, if any on a letter */
-    then serverSocket.connection.map(Timer.Connect(_))
+    then serverSocket.connection.map(Listener.Connect(_))
     /* ... so then */
     else
       /* ... report the problem */
       Logger.warn(s"Exception on ServerSocket: ${serverSocket.error}")
       /* ... stop the server by sending the termination letter. */
-      Some(Timer.Terminated)
+      Some(Listener.Terminated)
 
   /* Try to open the socket on port 8180.  */
   serverSocket.open(8180)
@@ -97,37 +97,37 @@ class Timer extends RestrictActor(Timer,"server"), TimingAid, FamilyRoot(Timer),
   /* Handle all incoming letters. */
   protected def receive[Sender <: Accept](letter: Letter[Sender], sender: Sender): Unit = letter match
     /* The new connection will come in as a letter. */
-    case Timer.Connect(socket) =>
+    case Listener.Connect(socket) =>
       Logger.info("Accepted a connection.")
       /* We see the providers as workers and generate automatic names for them. */
       val provider = new Provider(socket,this)
       /* Be ready for a new connection. */
       if !useCallback then expect(connect,expectationAnchor)
     /* The request has come to close stop this server. */
-    case Timer.Terminated =>
-      Logger.info("Timer Termination Request")
+    case Listener.Terminated =>
+      Logger.info("Listener Termination Request")
       /* Cancel the expectation for a new connection.
        * BTW, this is automatic in stopDirect, for illustration only. */
       dump(expectationAnchor)
       /* Stop the actor. */
       stop(Actor.Stop.Direct)
 
-  protected override def except[Sender >: Common <: Accept](letter: Timer.Letter[Sender], sender: Sender, cause: Exception, size: Int): Unit =
+  protected override def except[Sender >: Common <: Accept](letter: Listener.Letter[Sender], sender: Sender, cause: Exception, size: Int): Unit =
     Logger.warn(s"Exception Occurred: ${cause.getMessage()}")
 
   override def stopped(cause: Actor.Stop, complete: Boolean) =
-    println("Timer stopped")
+    println("Listener stopped")
     /* Decently close this socket. */
     serverSocket.close()
 
 
 /* This is the natural location to define all the letters the actor may receive. */
-object Timer extends RestrictDefine, FamilyDefine, Stateless :
+object Listener extends RestrictDefine, FamilyDefine, Stateless :
   // TODO: At the moment there are no common messages, extend!
-  type FamilyAccept = Timer & Provider
+  type FamilyAccept = Listener & Provider
   type FamilyLetter[Sender <: FamilyAccept] = Nothing
-  type Accept = Timer | Anonymous //| Actor
-  /* Base type of all Timer Letters, sealed because that enables the compiler to see
+  type Accept = Listener | Anonymous //| Actor
+  /* Base type of all Listener Letters, sealed because that enables the compiler to see
    * if we handled them all. */
   sealed trait Letter[Sender <: Accept] extends Actor.Letter[Sender]
   /* Letter that transfers a connection */
