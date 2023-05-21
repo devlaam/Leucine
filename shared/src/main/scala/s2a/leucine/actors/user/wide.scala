fb2753422e589ccd082707702777be60fddb7293
devlaam <ruud@betaresearch.nl>
2023-05-21 09:46:28 +0200

    Added the WideActor

---
+++ shared/src/main/scala/s2a/leucine/actors/user/wide.scala
@@ -0,0 +1,131 @@
+package s2a.leucine.actors
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2023 Ruud Vlaming
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
+
+/**
+ * The WideActor accepts messages from any other actor, and any message that is derived from Actor.Letter, also when they
+ * are defined or meant for other actors. In fact, this renders the typed aspect for typed actors a little useless. But
+ * it is mean to make the transition from non typed to typed actor design easier. Senders are tracked.
+ * If no name is given, an unique name is generated, but the actor is not indexed
+ * to be retrieved on the base of its name. Supply !# as name to define this a worker actor. Supply the (companion) object which
+ * contains the necessary type aliases as first parameter. */
+abstract class WideActor[Define <: WideDefine](private[actors] val actorDefine: Define, prename: String = "")(using val context: ActorContext) extends BareActor, ActorShare(prename) :
+
+  type Accept = Actor | this.type
+  type Common = Actor
+  type State  = actorDefine.State
+  type Letter = MyLetter[Actor]
+  type Sender = Actor
+  private[actors] type MyLetter[Sender >: Common <: Accept] = Actor.Letter[Actor]
+
+  /* Deliver the letter in the envelope. The state remains unchanged. */
+  private[actors] final def deliverEnvelope[Sender >: Common <: Accept](envelope: Env[Sender], state: State): State =
+    /* Let the letter be processed */
+    val received = receive(envelope.letter,envelope.sender)
+    /* The state remains unchanged, if we work stateless, otherwise compute the new state.
+     * TODO: Can this also be solved compile time? In an elegant manner?
+     * Based on this: https://scastie.scala-lang.org/13dD1LD8Q3OUpLrn89oLqw? */
+    if received.isInstanceOf[Unit] then state else received.asInstanceOf[State => State](state)
+
+  /* Process the exception to the user. The state remains unchanged. */
+  private[actors] final def deliverException[Sender >: Common <: Accept](envelope: Env[Sender], state: State, exception: Exception, exceptionCounter: Int): State =
+    except(envelope.letter,envelope.sender,exception,exceptionCounter)
+    state
+
+  /* Defines the initialState to be the Default state, the user does not need to implement this. */
+  private[actors] final def initialState: State = actorDefine.initial
+
+  /* Use to distinguish between basic and other actors. AcceptActors does not have sender as parameter. */
+  extension (fc: FamilyRelay {  type FamilyAccept = Actor; type Sender = Actor;  } )
+    /**
+     * Forward a message to all children with indexed name, workers and auto named.
+     * Returns the number of children that accepted the letter. Does not include
+     * auto named children (children that were not given an explicit name) or workers. */
+    protected def relayAll(letter: fc.MyFamilyLetter[fc.FamilyAccept], sender: fc.Sender): Int =
+      fc.relayEnvGrouped(letter,sender,true,true,true)
+     /**
+     * Forward a message to children of which the name passes the test 'include'.
+     * Returns the number of children that accepted the letter. Does not include
+     * auto named children (children that were not given an explicit name) or workers. */
+    protected def relayFilter(letter: fc.MyFamilyLetter[fc.FamilyAccept], sender: fc.Sender, include: String => Boolean): Int =
+      fc.relayEnvFilter(letter,sender,include)
+    /**
+     * Forward a message to children per group: indexed and/or workers and/or children that were given
+     * an automatic name, i.e. children that were not given an explicit name.
+     * Returns the number of children that accepted the letter.  */
+    protected def relayGrouped(letter: fc.MyFamilyLetter[fc.FamilyAccept], sender: fc.Sender, toIndexed: Boolean, toWorkers: Boolean, toAutoNamed: Boolean): Int =
+      fc.relayEnvGrouped(letter,sender,toIndexed,toWorkers,toAutoNamed)
+    /**
+     * Forward a message to one specific child on the basis of its name. Returns true if successful and
+     * false if that child is not present or does not accept the letter. */
+    protected def pass(letter: fc.MyFamilyLetter[fc.FamilyAccept], sender: fc.Sender, name: String): Boolean = fc.passEnv(letter,sender,name)
+
+  extension (stash: StashOps)
+    /**
+     * Store a letter and sender manually on the stash. With this method, you may replace one
+     * letter with an other, or spoof the sender, and reprocess later. If the actor was asked to
+     * finish, store will still work, since the letter was from before that request. */
+    protected def store(letter: Letter, sender: Sender): Unit = stash.storeEnv(pack(letter,sender))
+
+  /**
+   * Implement this method in your actor to process the letters send to you. There sender contains a reference
+   * to the actor that send the message. To be able to return an answer, you must know the original actor type.
+   * This can be obtained by a runtime type match. Use the send method on the senders matched type.  */
+  protected def receive(letter: Letter, sender: Sender): Receive
+
+  /**
+   * Override this in your actor to process exceptions that occur while processing the letters. The default implementation
+   * is to ignore the exception and pass on to the next letter. The size is the total number of exceptions this actor
+   * experienced. You may decide to:
+   * (1) Stop the actor, by calling stopDirect() inside the handler.
+   * (2) Continue for all or certain types of exceptions.
+   * (3) Inform the parent if part of a family...
+   * This can all be defined in this handler, so there is no need to configure some general actor behavior. If actors
+   * can be grouped with respect to the way exceptions are handled, you may define this in your CustomActor mixin, for
+   * example, just log the exception. Runtime errors cannot be caught and bubble up. */
+  protected def except(letter: Letter, sender: Sender, cause: Exception, size: Int): Unit = ()
+
+  /**
+   * Send a letter, with the option to say who is sending it. Defaults to anonymous outside the context
+   * of an actor and to self inside an actor. Returns if the letter was accepted for delivery. Note, this
+   * does not mean it also processed. In the mean time the actor may stop. */
+  def send(letter: Letter, sender: Sender): Boolean = sendEnvelope(pack(letter,sender))
+
+  /** Send a letter with the 'tell' operator. For compatibility with Akka. */
+  def !(letter: Letter)(using sender: Sender): Unit = sendEnvelope(pack(letter,sender))
+
+
+/** Derive your companion object from this trait, so you can define your own typed letters. */
+trait WideDefine :
+  /** Define the State you want to modify. Note: if you do not want/have this, mixin Stateless. */
+  type State <: Actor.State
+  /** Your class may contain a sealed trait Letter derived from Actor.Letter[Accept]. */
+  type Letter <: Actor.Letter[Actor]
+  /** Use this inside the actor to allow the anonymous sender in Accept */
+  type Anonymous = Actor.Anonymous.type
+  /** Define the initial value of the state. */
+  def initial: State
+
---
+++ shared/src/test/scala/s2a/leucine/actors/user/wide.scala
@@ -0,0 +1,92 @@
+package s2a.leucine.actors
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.DurationInt
+import utest.*
+
+import s2a.control.{Buffer, Deferred}
+
+object WideActorTest extends TestSuite :
+
+  implicit val ac: ActorContext = ActorContext.system
+
+
+  class Joni(val writeln: String => Unit, val done: () => Unit) extends WideActor(Joni,"Joni") :
+    var mary: Option[Mary] = None
+    var sara: Option[Sara] = None
+    def sendJoni(text: String, depth: Int) = this ! Common.Text(s"${text}J",depth+1)
+    def sendMary(text: String, depth: Int) = mary.foreach(_ ! Common.Text(s"${text}J",depth+1))
+    def sendSara(text: String, depth: Int) = sara.foreach(_ ! Common.Text(s"${text}J",depth+1))
+    def receive(letter: Letter, sender: Sender): Unit = (letter,sender) match
+      case (Common.Config(_,mary,sara),_)      => this.mary=Some(mary); this.sara=Some(sara)
+      case (Common.Text(text,depth), _: Joni)  => writeln(s"$text$depth"); done()
+      case (Common.Text(text,depth), _: Mary)  => if depth< 5 then sendSara(text,depth) else sendJoni(text,depth)
+      case (Common.Text(text,depth), _: Sara)  => if depth< 5 then sendMary(text,depth) else sendJoni(text,depth)
+      case (Common.Text(_,0), _ : Anonymous)   => sendMary("*",0); sendSara("*",0)
+      case (Common.Text(_,_), _ : Anonymous)   => writeln(s"Unhandled case $letter: $sender")
+
+  object Joni extends WideDefine, Stateless
+
+
+
+  class Mary(val writeln: String => Unit, val done: () => Unit) extends WideActor(Mary,"Mary") :
+    var joni: Option[Joni] = None
+    var sara: Option[Sara] = None
+    def sendJoni(text: String, depth: Int) = joni.foreach(_ ! Common.Text(s"${text}M",depth+1))
+    def sendMary(text: String, depth: Int) = this ! Common.Text(s"${text}M",depth+1)
+    def sendSara(text: String, depth: Int) = sara.foreach(_ ! Common.Text(s"${text}M",depth+1))
+
+    def receive(letter: Letter, sender: Sender): Unit = (letter,sender) match
+      case (Common.Config(joni,_,sara),_)      => this.joni=Some(joni); this.sara=Some(sara)
+      case (Common.Text(text,depth), _: Joni)  => if depth< 5 then sendSara(text,depth) else sendMary(text,depth)
+      case (Common.Text(text,depth), _: Mary)  => writeln(s"$text$depth"); done()
+      case (Common.Text(text,depth), _: Sara)  => if depth< 5 then sendJoni(text,depth) else sendMary(text,depth)
+      case (Common.Text(_,0), _ : Anonymous)   => sendJoni("*",0); sendSara("*",0)
+      case (Common.Text(_,_), _ : Anonymous)   => writeln(s"Unhandled case $letter: $sender")
+
+  object Mary extends WideDefine, Stateless
+
+
+  class Sara(val writeln: String => Unit, val done: () => Unit) extends WideActor(Sara,"Sara") :
+    var joni: Option[Joni] = None
+    var mary: Option[Mary] = None
+    def sendJoni(text: String, depth: Int) = joni.foreach(_ ! Common.Text(s"${text}S",depth+1))
+    def sendMary(text: String, depth: Int) = mary.foreach(_ ! Common.Text(s"${text}S",depth+1))
+    def sendSara(text: String, depth: Int) = this ! Common.Text(s"${text}S",depth+1)
+
+    def receive(letter: Letter, sender: Sender): Unit = (letter,sender) match
+      case (Common.Config(joni,mary,_),_)      => this.joni=Some(joni); this.mary=Some(mary)
+      case (Common.Text(text,depth), _: Joni)  => if depth< 5 then sendMary(text,depth) else sendSara(text,depth)
+      case (Common.Text(text,depth), _: Mary)  => if depth< 5 then sendJoni(text,depth) else sendSara(text,depth)
+      case (Common.Text(text,depth), _: Sara)  => writeln(s"$text$depth"); done()
+      case (Common.Text(_,0), _ : Anonymous)   => sendJoni("*",0); sendMary("*",0)
+      case (Common.Text(_,_), _ : Anonymous)   => writeln(s"Unhandled case $letter: $sender")
+
+  object Sara extends WideDefine, Stateless
+
+  object Common :
+    sealed trait Letter extends Actor.Letter[Actor]
+    case class Config(joni: Joni, mary: Mary, sara: Sara) extends Letter
+    case class Text(text: String, depth: Int) extends Letter
+
+
+
+  val tests = Tests {
+    val expect = Set("*JMSJMS6", "*MJSMJS6", "*JSMJSM6", "*MSJMSJ6", "*SMJSMJ6", "*SJMSJM6")
+    given Actor.Anonymous = Actor.Anonymous
+    test("sending looped letters"){
+      val buffer = Buffer[String]
+      val deferred = Deferred(buffer.readlns,6)
+      val joni = Joni(buffer.writeln,deferred.done)
+      val mary = Mary(buffer.writeln,deferred.done)
+      val sara = Sara(buffer.writeln,deferred.done)
+      joni ! Common.Config(joni,mary,sara)
+      mary ! Common.Config(joni,mary,sara)
+      sara ! Common.Config(joni,mary,sara)
+      joni ! Common.Text("",0)
+      mary ! Common.Text("",0)
+      sara ! Common.Text("",0)
+      deferred.await()
+      deferred.compare(_.toSet ==> expect) }
+   }
